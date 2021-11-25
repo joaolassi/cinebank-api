a3dcf72a144b81fc0d8c49582a50c8fb6ae94390
Joao Lassi <jmlassi@outlook.com>
2021-11-25 18:27:49 -0300

    Envio da api

--- src/main/resources/db/migration/V02__criar_coluna_imagem_tb_filme.sql
+++ src/main/resources/db/migration/V02__criar_coluna_imagem_tb_filme.sql
@@ -1,5 +1,1 @@
 ALTER TABLE filme ADD COLUMN imagem VARCHAR(200) NOT NULL;
-
-INSERT INTO filme (titulo, descricao, duracao, imagem) values ('O Senhor Dos Anéis: A Sociedade do Anel', 'Um filme baseado na obra de J.R.R Tolkien', '2:58', '2649d0bc-e8a6-448b-9b19-8fbf80397b31_lotrsociedade.jpg');
-INSERT INTO filme (titulo, descricao, duracao, imagem) values ('O Senhor Dos Anéis: As Duas Torres', 'Um filme baseado na obra de J.R.R Tolkien', '2:59', '18ed4ed3-fb6a-417f-a8a6-0a830f110290_lotrasduastorres.jpg');
-INSERT INTO filme (titulo, descricao, duracao, imagem) values ('O Senhor Dos Anéis: O Retorno do Rei', 'Um filme baseado na obra de J.R.R Tolkien', '3:21', '5c2384ee-d97f-4314-8bdf-7ab68302a5be_lotrretornodorei.jpg');
--- src/main/resources/db/migration/V04__criar_tabela_sessao.sql
+++ src/main/resources/db/migration/V04__criar_tabela_sessao.sql
@@ -11,7 +11,3 @@ CREATE TABLE sessao(
     FOREIGN KEY (codigo_filme) REFERENCES filme(codigo),
     FOREIGN KEY (codigo_sala) REFERENCES sala(codigo)
 )ENGINE=InnoDB DEFAULT CHARSET=utf8;
-
-INSERT INTO sessao (codigo, data, hora_inicio, hora_fim, valor, tipo_animacao, tipo_audio, codigo_filme, codigo_sala) values(1, '2022-11-04' , '13:00', '15:58', 35.00, 'ANIMACAO2D', 'ORIGINAL', 1, 1);
-INSERT INTO sessao (codigo, data, hora_inicio, hora_fim, valor, tipo_animacao, tipo_audio, codigo_filme, codigo_sala) values(2, '2022-11-04' , '15:30', '18:29', 60.00, 'ANIMACAO3D', 'ORIGINAL', 2, 2);
-INSERT INTO sessao (codigo, data, hora_inicio, hora_fim, valor, tipo_animacao, tipo_audio, codigo_filme, codigo_sala) values(3, '2022-11-04' , '20:00', '23:21', 35.00, 'ANIMACAO2D', 'DUBLADO', 3, 4);
